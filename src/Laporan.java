29def24f1486fa4ef67ad65a5b479e47d39ff667
unknown <afi.firda@gmail.com>
2022-04-26 18:35:17 +0700

    add - designing the output

--- src/Laporan.java
+++ src/Laporan.java
@@ -1,39 +1,39 @@
 public class Laporan {
 
-    public void laporan(Client client){
+    public void laporan(Client client) {
         int cl = client.getJumlahClient();
-        System.out.println("Tabel Nama client ");
-        System.out.println("Nama \t Alamat \t No. Telp \t Saldo");
+        System.out.println("\n=======Tabel Nama client=======");
+        System.out.println("No. Nama \tAlamat \t\tNo. Telp \t\tSaldo");
         for (int i = 0; i < cl; i++) {
-            System.out.println(client.getNamaClient(i) + "\t" + client.getAlamat(i) + "\t" + client.getTelepon(i) + "\t" + client.getSaldo(i));
+            System.out.println(i + 1 + "\t" + client.getNamaClient(i) + "\t" + client.getAlamat(i) + "\t" + client.getTelepon(i) + "\t" + client.getSaldo(i));
         }
     }
 
-    public void laporan(Petugas petugas){
+    public void laporan(Petugas petugas) {
         int pt = petugas.getJumlahPetugas();
-        System.out.println("Tabel Nama Petugas");
-        System.out.println("Nama \t Alamat \t No. Telp \t Saldo");
+        System.out.println("\n=======Tabel Nama Petugas=======");
+        System.out.println("No. Nama \t\tAlamat \t\tNo. Telp \t\tJabatan");
         for (int i = 0; i < pt; i++) {
-            System.out.println(petugas.getNamaPetugas(i) + "\t" + petugas.getAlamat(i) + "\t" + petugas.getTelepon(i) + "\t" + petugas.getJabatan(i));
+            System.out.println(i + 1 + "\t" + petugas.getNamaPetugas(i) + "\t" + petugas.getAlamat(i) + "\t" + petugas.getTelepon(i) + "\t" + petugas.getJabatan(i));
         }
     }
 
-    public void laporan(jenisLaundry laundry){
+    public void laporan(jenisLaundry laundry) {
         int ldy = laundry.getJumlahJenisLaundry();
-        System.out.println("Tabel Jenis Laundry");
-        System.out.println("Jenis Laundry \t harga \t durasi");
+        System.out.println("\n=======Tabel Jenis Laundry=======");
+        System.out.println("No. Jenis Laundry \tharga \tdurasi");
         for (int i = 0; i < ldy; i++) {
-            System.out.println(laundry.getJenisLaundry(i) + "\t" + laundry.getHarga(i) + "\t" + laundry.getDurasi(i));
+            System.out.println(i + 1 + "\t" + laundry.getJenisLaundry(i) + "\t\t\t" + laundry.getHarga(i) + "\t" + laundry.getDurasi(i));
         }
     }
 
-    public void laporan(Transaksi transaksi, jenisLaundry laundry, Client client){
+    public void laporan(Transaksi transaksi, jenisLaundry laundry, Client client) {
         int trsk = transaksi.getJumlahTransaksi();
 
-        System.out.println("Transaksi yang terjadi");
-        System.out.println("Nama \t Jenis laundry \t harga \t durasi \t total");
+        System.out.println("\n=======Transaksi yang terjadi=======");
+        System.out.println("No. Nama \tJenis laundry \tBerat \tharga \tdurasi \ttotal");
         for (int i = 0; i < trsk; i++) {
-            System.out.println(client.getNamaClient(i) + "\t" + laundry.getJenisLaundry(i) + "\t" + transaksi.getBanyak(i) + "\t" + laundry.getHarga(i) + "\t" + laundry.getDurasi(i) + "\t" + (laundry.getHarga(i) * transaksi.getBanyak(i)));
+            System.out.println(i + 1 + "\t" + client.getNamaClient(i) + "\t" + laundry.getJenisLaundry(i) + "\t\t\t" + transaksi.getBanyak(i) + "\t\t" + laundry.getHarga(i) + "\t" + laundry.getDurasi(i) + "\t\t" + (laundry.getHarga(i) * transaksi.getBanyak(i)));
         }
     }
 }
--- src/Transaksi.java
+++ src/Transaksi.java
@@ -34,19 +34,20 @@ public class Transaksi {
     }
 
     public void prosesTransaksi(Client client, jenisLaundry laundry) {
-        System.out.println("PROSES LAUNDRY");
-        System.out.println("Nama : " + client.getNamaClient(idClient.get(0)));
-        System.out.println("Alamat : " + client.getAlamat(idClient.get(0)));
-        System.out.println("Telepon : " + client.getTelepon(idClient.get(0)));
-        System.out.println("Saldo : " + client.getSaldo(idClient.get(0)));
+        System.out.println("\n=======PROSES LAUNDRY=======");
+        System.out.println("Nama          : " + client.getNamaClient(idClient.get(0)));
+        System.out.println("Alamat        : " + client.getAlamat(idClient.get(0)));
+        System.out.println("Telepon       : " + client.getTelepon(idClient.get(0)));
+        System.out.println("Saldo         : " + client.getSaldo(idClient.get(0)));
         System.out.println("Jenis Laundry : " + laundry.getJenisLaundry(idJenisLaundry.get(0)));
-        System.out.println("Harga : " + laundry.getHarga(idJenisLaundry.get(0)) * banyak.get(0));
-        System.out.println("Durasi : " + laundry.getDurasi(idJenisLaundry.get(0)));
+        System.out.println("Harga         : " + laundry.getHarga(idJenisLaundry.get(0)) * banyak.get(0));
+        System.out.println("Durasi        : " + laundry.getDurasi(idJenisLaundry.get(0)));
 
-        globalTemp = client.getSaldo(idClient.get(0)) - (laundry.getHarga(0) * banyak.get(0));
+        globalTemp = client.getSaldo(idClient.get(0)) - (laundry.getHarga(idJenisLaundry.get(0)) * banyak.get(0));
 
-        System.out.println("Saldo anda menjadi : " + globalTemp);
-        System.out.println("TERIMAKASIH");
+        System.out.println("Saldo akhir   : " + globalTemp);
+        System.out.println("=========TERIMAKASIH=========");
+        System.out.println("=TELAH MENGGUNAKAN JASA KAMI=");
     }
 
     public void start(Client client, Petugas petugas, jenisLaundry laundry, Laporan laporan, Transaksi transaksi) {
@@ -55,6 +56,7 @@ public class Transaksi {
         laporan.laporan(laundry);
         laporan.laporan(transaksi, laundry, client);
 
+        System.out.println();
         inputTransaksi(client, laundry);
         prosesTransaksi(client, laundry);
 
